7f302e7f53fb6cecfff03d343b7144a1f23b7c67
apprehen <3514392356@qq.com>
2023-05-04 21:10:03 +0800

    update

---
+++ lovely_front/src/components/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+const a = 1
+const b = ref(2)
+console.log(a, b)
+</script>
+<style scoped lang='less'></style>
