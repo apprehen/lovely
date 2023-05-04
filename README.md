74d7d25e2ff965f3dc196fd28c22b269ba88da0c
apprehen <3514392356@qq.com>
2023-05-04 08:20:02 +0800

    update

--- README.md
+++
@@ -1,9 +0,0 @@
-# 创建项目
-> yarn create vue
-> (ts + pinia + router)
-> yarn
-> yarn format
-> yarn dev
-
-# 项目介绍
-**杀软二次元平台正在开发中捏**
---
+++ lovely_front/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="test">EXPLOSION
+		<div class="innerTest">hhh</div>
+	</div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+
+</script>
+<style scoped lang='less'>
+.test {
+	color: red;
+
+	.innerTest {
+		color: blue;
+	}
+}
+</style>
+
